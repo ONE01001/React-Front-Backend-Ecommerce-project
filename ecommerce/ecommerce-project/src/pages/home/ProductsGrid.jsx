ad305c850278da96e3f90dddfd6b8a296ae5c1c2
Hardik rana <hardikrana2717@gmail.com>
2026-04-30 21:58:19 +0530

    refactor: format "Add to Cart" button code for improved readability in ProductsGrid component

--- ecommerce/ecommerce-project/src/pages/home/ProductsGrid.jsx
+++ ecommerce/ecommerce-project/src/pages/home/ProductsGrid.jsx
@@ -52,13 +52,14 @@ export function ProductsGrid({ products }) {
                 Added
               </div>
 
-              <button className="add-to-cart-button button-primary"
-              onClick={()=>{
-                axios.post('/api/cart-items' ,{
-                  productId: product.id   ,
-                  quantity: 1
-                })
-              }}
+              <button
+                className="add-to-cart-button button-primary"
+                onClick={() => {
+                  axios.post("/api/cart-items", {
+                    productId: product.id,
+                    quantity: 1,
+                  });
+                }}
               >
                 Add to Cart
               </button>
